c9a0d62930a10894695892ad910161c0c49dfe0c
CampbellGibsonMQ2021_46552375 <Campbell.Gibson@students.mq.edu.au>
2023-05-15 09:30:59 +1000

    update inline with other file

--- test_app.py
+++ test_app.py
@@ -36,7 +36,9 @@ class TestWebApp(unittest.TestCase):
 
     def test_no_access_to_profile(self):
         # TODO: Check that non-logged-in user should be redirected to /login
-        assert False
+        response = self.client.get('/profile', follow_redirects = True)
+        assert response.status_code == 200
+        assert response.request.path == '/login'
 
     def test_register_user(self):
         response = self.client.post('/signup', data = {
@@ -81,6 +83,12 @@ class TestWebApp(unittest.TestCase):
 
     def test_xss_vulnerability(self):
         # TODO: Can we store javascript tags in the username field?
-        assert False
+        XSS = '<script>alert(‘XSS’)</script>'
+        response = self.client.post('/signup', data = {
+        'email':'test@example.com',
+        'name': XSS,
+        'password':'test'
+        }, follow_redirects=True)
+        assert XSS not in response.data.decode('utf-8')
 
 
